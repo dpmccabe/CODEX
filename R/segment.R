39de610b11f530b89fdf9aad4ca2ed4df7dfd60f
Devin McCabe <dmccabe@broadinstitute.org>
2016-07-29 15:32:35 -0400

    refactor segment

--- R/segment.R
+++ R/segment.R
@@ -1,101 +1,94 @@
 segment <- function(Y_qc, Yhat, optK, K, sampname_qc, ref_qc, chr, lmax, mode) {
-    finalcall <- matrix(ncol = 9)
-    lmax <- lmax - 1
-    for (sampno in 1:ncol(Y_qc)) {
-        message("Segmenting sample ", sampno, ": ", sampname_qc[sampno], ".")
-        y <- Y_qc[, sampno]
-        yhat <- Yhat[[which(K == optK)]][, sampno]
-        num <- length(y)
-        y <- c(y, rep(0, lmax))
-        yhat <- c(yhat, rep(0, lmax))
-        i <- rep(1:num, rep(lmax, num))
-        j <- rep(1:lmax, num) + i
-        yact <- rep(0, length(i))
-        lambda <- rep(0, length(i))
-        for (k in 1:num) {
-            yact[(lmax * k - (lmax - 1)):(lmax * k)] <- cumsum(y[k:(k + 
-                lmax)])[-1]
-            lambda[(lmax * k - (lmax - 1)):(lmax * k)] <- cumsum(yhat[k:(k + 
-                lmax)])[-1]
-        }
-        i <- i[j <= num]
-        yact <- yact[j <= num]
-        lambda <- lambda[j <= num]
-        j <- j[j <= num]
-        yact[lambda<20] <- 20
-        lambda[lambda<20] <- 20
-        if (mode == "integer") {
-            chat <- round(2 * (yact/lambda))
-        } else if (mode == "fraction") {
-            chat <- 2 * (yact/lambda)
-        }
-        lratio <- (1 - chat/2) * lambda + log((chat + 1e-04)/2.0001) * yact
-        chat[chat > 5] <- 5
-        if (sum(lratio > 0) > 0) {
-            if (sum(lratio > 0) >= 2) {
-                finalmat <- (cbind(i, j, yact, lambda, chat, lratio))[lratio > 
-                    0, ]
-                finalmat <- finalmat[order(-finalmat[, 6]), ]
-                s <- 1
-                while (s <= (nrow(finalmat))) {
-                    rowstart <- finalmat[s, 1]
-                    rowend <- finalmat[s, 2]
-                    rowsel <- (finalmat[, 1] <= rowend & finalmat[, 2] >= 
-                        rowstart)
-                    rowsel[s] <- FALSE
-                    finalmat <- finalmat[!rowsel, ]
-                    if (is.vector(finalmat)) {
-                        finalmat <- t(as.matrix(finalmat))
-                    }
-                    s <- s + 1
-                }
-            }
-            if (sum(lratio > 0) == 1) {
-                finalmat <- (cbind(i, j, yact, lambda, chat, lratio))[lratio > 
-                    0, ]
-                finalmat <- t(as.matrix(finalmat))
-            }
-            finalmat <- round(finalmat, digits = 3)
-            loglikeij <- cumsum(finalmat[, 6])
-            mBIC <- rep(NA, length(loglikeij))
-            for (s in 1:nrow(finalmat)) {
-                tau <- sort(unique(c(as.vector(finalmat[1:s, 1:2]), 1, num)))
-                P <- length(tau) - 2
-                mbic <- loglikeij[s]
-                mbic <- mbic - 0.5 * sum(log(tau[2:length(tau)] - 
-                    tau[1:(length(tau) - 1)]))
-                mbic <- mbic + (0.5 - P) * log(num)
-                mBIC[s] <- mbic
-            }
-            mBIC <- round(mBIC, digits = 3)
-            if (mBIC[1] > 0) {
-                finalmat <- cbind(rep(sampname_qc[sampno], nrow(finalmat)), 
-                    rep(chr, nrow(finalmat)), finalmat)
-                finalmat <- (cbind(finalmat, mBIC)[1:which.max(mBIC), ])
-                finalcall <- rbind(finalcall, finalmat)
-            }
-        }
+  lmax <- lmax - 1
+  
+  res <- adply(1:ncol(Y_qc), 1, function(sampno) {
+    message("Segmenting sample ", sampno, ": ", sampname_qc[sampno], ".")
+    y <- Y_qc[, sampno]
+    yhat <- Yhat[[which(K == optK)]][, sampno]
+    num <- length(y)
+    y <- c(y, rep(0, lmax))
+    yhat <- c(yhat, rep(0, lmax))
+    i <- rep(1:num, rep(lmax, num))
+    j <- rep(1:lmax, num) + i
+    yact <- rep(0, length(i))
+    lambda <- rep(0, length(i))
+    
+    for (k in 1:num) {
+      yact[(lmax * k - (lmax - 1)):(lmax * k)] <- cumsum(y[k:(k + lmax)])[-1]
+      lambda[(lmax * k - (lmax - 1)):(lmax * k)] <- cumsum(yhat[k:(k + lmax)])[-1]
     }
-    finalcall <- finalcall[-1, ]
-    if (is.vector(finalcall)) {
-        finalcall <- t(as.matrix(finalcall))
+    
+    i <- i[j <= num]
+    yact <- yact[j <= num]
+    lambda <- lambda[j <= num]
+    j <- j[j <= num]
+    yact[lambda < 20] <- 20
+    lambda[lambda < 20] <- 20
+    
+    if (mode == "integer") {
+      chat <- round(2 * (yact / lambda))
+    } else if (mode == "fraction") {
+      chat <- 2 * (yact / lambda)
     }
-    st <- start(ref_qc)[as.numeric(finalcall[, 3])]
-    ed <- end(ref_qc)[as.numeric(finalcall[, 4])]
-    cnvtype <- rep(NA, length(st))
-    cnvtype[finalcall[, 7] < 2] <- "del"
-    cnvtype[finalcall[, 7] > 2] <- "dup"
-    if (nrow(finalcall) == 1) {
-        finalcall <- t(as.matrix(c(finalcall[, 1:2], cnvtype, st, ed, (ed - 
-            st + 1)/1000, finalcall[, 3:9])))
+    
+    lratio <- (1 - chat / 2) * lambda + log((chat + 1e-04) / 2.0001) * yact
+    chat[chat > 5] <- 5
+    
+    samp_data <- data.table(samp = sampnames[sampno], chr = chr, i, j, yact, lambda, chat, lratio)[lratio > 0, ]
+    
+    if (sum(lratio > 0) > 0) {
+      if (sum(lratio > 0) >= 2) {
+        samp_data <- samp_data[order(-lratio)]
+        s <- 1
+        
+        while (s <= (nrow(samp_data))) {
+          samp_data <- rbind(samp_data[s], samp_data[i > samp_data[s, j] | j < samp_data[s, i]])
+          s <- s + 1
+        }
+      } else if (sum(lratio > 0) != 1) {
+        stop("unexpected sum(lratio > 0)")
+      }
+      
+      samp_data <- samp_data[order(-lratio)]
+      loglikeij <- cumsum(samp_data$lratio)
+
+      mBIC <- aaply(1:nrow(samp_data), 1, function(s) {
+        tau <- sort(unique(c(samp_data[1:s, c(i, j)], 1, num)))
+        mbic <- loglikeij[s]
+        mbic <- mbic - 0.5 * sum(log(tau[2:length(tau)] - tau[1:(length(tau) - 1)]))
+        mbic <- mbic + (0.5 - (length(tau) - 2)) * log(num)
+        return(mbic)
+      }, .parallel = T)
+
+      bic_ind <- 1:which.max(mBIC)
+      samp_data <- samp_data[bic_ind]
+      samp_data$mbic <- mBIC[bic_ind]
     } else {
-        finalcall <- cbind(finalcall[, 1:2], cnvtype, st, ed, (ed - st + 
-            1)/1000, finalcall[, 3:9])
+      stop("unexpected sum(lratio > 0)")
     }
-    colnames(finalcall) <- c("sample_name", "chr", "cnv", "st_bp", "ed_bp", 
-        "length_kb", "st_exon", "ed_exon", "raw_cov", 
-        "norm_cov", "copy_no", "lratio", "mBIC")
-    rownames(finalcall) <- rep("", nrow(finalcall))
-    lratio=as.numeric(finalcall[,'lratio'])
-    finalcall
+    
+    return(samp_data)
+  }, .parallel = F, .id = NULL)
+  
+  res <- subset(res, mbic > 0)
+  
+  ret_cols <- c("sample_name", "chr", "cnv", "st_bp", "ed_bp", "length_kb", "st_exon", "ed_exon", "raw_cov", "norm_cov", "lratio", "mBIC")
+
+  if (nrow(res) > 0) {
+    res$st_bp <- start(ref_qc)[res$i]
+    res$ed_bp <- end(ref_qc)[res$j]
+
+    res$cnv <- NA
+    res[res$chat < 2, "cnv"] <- "del"
+    res[res$chat > 2, "cnv"] <- "dup"
+    res$cnv <- as.factor(res$cnv)
+    
+    res$length_kb <- (res$ed_bp - res$st_bp + 1) / 1000
+    
+    colnames(res) <- c("sample_name", "chr", "st_exon", "ed_exon", "raw_cov", "norm_cov", "copy_no", "lratio", "mBIC", "st_bp", "ed_bp", "cnv", "length_kb")
+    
+    return(res[, ret_cols])
+  } else {
+    return(NULL)
+  }
 }
